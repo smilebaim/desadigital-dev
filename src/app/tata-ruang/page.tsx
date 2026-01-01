078964858186ec3b01f57c1e161a372d7a28b441
Maintenance Web <maintenance.web@gmail.com>
2026-01-01 12:30:01 +0000

    perbaiki map dasar leaflet di "/tata-ruang"

--- src/app/tata-ruang/page.tsx
+++ src/app/tata-ruang/page.tsx
@@ -3,26 +3,26 @@ import dynamic from 'next/dynamic';
 import TopNav from '@/components/TopNav';
 import BottomNav from '@/components/BottomNav';
 
-const MapComponentWithNoSSR = dynamic(
-  () => import('@/components/pages/tata-ruang/MapComponent'), 
-  {
+const MapComponent = dynamic(
+  () => import('@/components/pages/tata-ruang/MapComponent'),
+  { 
     ssr: false,
-    loading: () => <div className="w-full h-full flex items-center justify-center bg-gray-100">Memuat Peta...</div>
+    loading: () => <div className="fixed inset-0 bg-gray-200 flex items-center justify-center"><p>Memuat Peta...</p></div>
   }
 );
 
 export default function TataRuangPage() {
-  return (
-    <>
-      <div className="fixed top-0 left-0 right-0 z-[1000]">
-        <TopNav hasNewNews={false} />
-      </div>
-      <div className="fixed inset-0 pt-14 sm:pt-16 pb-14 sm:pb-16">
-        <MapComponentWithNoSSR />
-      </div>
-      <div className="fixed bottom-0 left-0 right-0 z-[1000]">
-        <BottomNav />
-      </div>
-    </>
-  );
+    return (
+      <>
+        <div className="fixed inset-0">
+          <MapComponent />
+        </div>
+        <div className="fixed top-0 left-0 right-0 z-[1000]">
+          <TopNav hasNewNews={false} />
+        </div>
+        <div className="fixed bottom-0 left-0 right-0 z-[1000]">
+          <BottomNav />
+        </div>
+      </>
+    )
 }
