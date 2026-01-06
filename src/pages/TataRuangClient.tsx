a7f28dbea6471ea7c2daf54385b0299bd38edf23
Maintenance Web <maintenance.web@gmail.com>
2026-01-06 18:32:22 +0000

    Try fixing this error: `Runtime Error: Error: Element type is invalid. R

--- src/pages/TataRuangClient.tsx
+++ src/pages/TataRuangClient.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
 import '@/styles/map.css';
 
-const MapComponent = dynamic(() => import('@/components/MapComponent').then(mod => mod.default), {
+const MapComponent = dynamic(() => import('@/components/MapComponent').then(mod => mod.MapComponent), {
   ssr: false,
   loading: () => <div className="flex items-center justify-center w-full h-full bg-gray-200"><p>Loading map...</p></div>
 });
