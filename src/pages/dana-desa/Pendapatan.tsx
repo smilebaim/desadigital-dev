59b580b7acaa055f196c81b7fe861a3daac8af7a
Maintenance Web <maintenance.web@gmail.com>
2026-01-08 08:12:56 +0000

    Try fixing this error: `Runtime Error: TypeError: Super expression must

--- src/pages/dana-desa/Pendapatan.tsx
+++ src/pages/dana-desa/Pendapatan.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
