dc087b3d9219774fe045451e8f361b1a0dbcfa16
Maintenance Web <maintenance.web@gmail.com>
2026-01-14 05:20:50 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://picsum.

--- next.config.js
+++ next.config.js
@@ -10,10 +10,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
